3c754c7f658a18cc88568bd589d46fbd36b7f16a
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2021-11-14 16:44:11 +0100

    Rearranged parameter order

--- filefilter.go
+++ filefilter.go
@@ -16,7 +16,7 @@ type Configuration struct {
 	Pattern *customtypes.Regexp `yaml:"pattern"`
 }
 
-func PassesFilter(logger *log.Entry, fileInfo fs.FileInfo, config *Configuration) (bool, string, error) {
+func PassesFilter(logger *log.Entry, config *Configuration, fileInfo fs.FileInfo) (bool, string, error) {
 	logger = logger.WithFields(log.Fields{"Package": "filefilter", "Function": "passesFilter", "Filter": config})
 	if config.Pattern != nil && !config.Pattern.Match([]byte(fileInfo.Name())) {
 		logger.Debugf("File did not pass the pattern filter")
@@ -33,7 +33,7 @@ func PassesFilter(logger *log.Entry, fileInfo fs.FileInfo, config *Configuration
 	return true, "", nil
 }
 
-func ReadDir(logger *log.Entry, dirPath string, config *Configuration) ([]fs.FileInfo, error) {
+func ReadDir(logger *log.Entry, config *Configuration, dirPath string) ([]fs.FileInfo, error) {
 	allFiles, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		return nil, fmt.Errorf("when listing files in the folder '%s': %w", dirPath, err)
@@ -42,7 +42,7 @@ func ReadDir(logger *log.Entry, dirPath string, config *Configuration) ([]fs.Fil
 	files := []fs.FileInfo{}
 	for _, fileInfo := range allFiles {
 		logger := logger.WithFields(log.Fields{"Filename": fileInfo.Name()})
-		passes, condition, err := PassesFilter(logger, fileInfo, config)
+		passes, condition, err := PassesFilter(logger, config, fileInfo)
 		if err != nil {
 			return nil, fmt.Errorf("when using the filter on the file '%s': %w", fileInfo.Name(), err)
 		}
